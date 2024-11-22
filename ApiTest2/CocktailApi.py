e4ab35ac16735e15d269d1b8f28726590812ebd4
LagBot5 <danefrancis1211@gmail.com>
2024-11-22 10:03:23 -0500

    Update CocktailApi.py

--- ApiTest2/CocktailApi.py
+++ ApiTest2/CocktailApi.py
@@ -1,20 +1,21 @@
-
-
 # Import libraries for http requests 
 # You must download the requests module before using this 
 import requests
 
 
 # Get all the cocktails
-response = requests.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=gin')
-print(response.status_code)
+response = 200 #requests.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=gin')
+print(response) #.status_code
+sad = true
 
 # If successful then do something with the result
-if response.status_code == 200:
-  Cocktails = response.json()
-  allDrinks = Cocktails['drinks']
-  for drink in allDrinks:
-    print(f" this is {drink['strDrink']}, and to make it you need to {drink['strInstructions']}")
+if response == 200:
+    # Cocktails = response.json
+    # allDrinks = Cocktails['drinks']
+    if sad == true:
+        
+        for drink in allDrinks:
+        print(f"I am sad so I need to drink it off so give me ", {drink['strAlcoholic']})
 
 # Otherwise show why the request was unsuccessful
 else:
